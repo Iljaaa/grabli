696432c922982b5df2239cbe828aee23a8cecf7d
galaichuk <the.ilja@gmail.com>
2013-11-20 11:18:00 +0400

    Модификация миграции

--- source/grabli/protected/migrations/m131113_164334_add_project_tags_table.php
+++ source/grabli/protected/migrations/m131113_164334_add_project_tags_table.php
@@ -5,20 +5,30 @@ class m131113_164334_add_project_tags_table extends CDbMigration
 	public function up()
 	{
 		$this->createTable('tags', array(
-			'id'    => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
-			'tag'   => 'varchar(255) NOT NULL',
+			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+			'project_id'    => 'int(11) UNSIGNED NOT NULL',
+			'tag'           => 'varchar(255) NOT NULL',
 		));
 
-		$this->createTable('project_tags', array(
+		$this->createTable('bugs_has_tags', array(
+			'issue_id'      => 'int(11) UNSIGNED NOT NULL',
+			'tag_id'        => 'int(11) UNSIGNED NOT NULL',
+		));
+
+		/* $this->createTable('project_tags', array(
 			'tag_id'        => 'int(11) UNSIGNED NOT NULL',
 			'project_id'    => 'int(11) UNSIGNED NOT NULL',
-		));
+		)); */
+
+
+
 	}
 
 	public function down()
 	{
 		$this->dropTable('tags');
-		$this->dropTable('project_tags');
+		$this->dropTable('bugs_has_tags');
+		// $this->dropTable('project_tags');
 
 
 		/*
