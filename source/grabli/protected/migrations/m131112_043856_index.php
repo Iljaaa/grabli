5e1314353c4d095415204c0e9feaa8c4b1ca7770
galaichuk <the.ilja@gmail.com>
2013-11-24 12:08:05 +0400

    Миграции бд

--- source/grabli/protected/migrations/m131112_043856_index.php
+++ source/grabli/protected/migrations/m131112_043856_index.php
@@ -4,6 +4,52 @@ class m131112_043856_index extends CDbMigration
 {
 	public function up()
 	{
+
+		$this->createTable('issues', array(
+			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+			'step_id'       => 'int(11) UNSIGNED NOT NULL',
+			'project_id'    => 'int(11) UNSIGNED NOT NULL',
+			'assigned_to'   => 'int(11) UNSIGNED NOT NULL',
+			'owner_id'      => 'int(11) UNSIGNED NOT NULL',
+			'parent_id'     => 'int(11) UNSIGNED NOT NULL',
+			'name'          => 'varchar(100) NOT NULL',
+			'type'          => 'varchar(20) NOT NULL',
+			'description'   => 'varchar(1000)',
+			'rep_steps'     => 'varchar(1000)',
+			'number'        => 'int(11) UNSIGNED NOT NULL',
+			'added_date'    => 'int(11) UNSIGNED',
+			'dedline_date'  => 'int(11) UNSIGNED',
+			'last_activity' => 'int(11) UNSIGNED',
+		));
+
+		$this->createTable('issue_has_issue', array(
+			'issue_1'       => 'int(11) UNSIGNED NOT NULL',
+			'issue_2'       => 'int(11) UNSIGNED NOT NULL',
+		));
+
+		$this->createTable('projects', array(
+			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+			'owner_id'      => 'int(11) UNSIGNED NOT NULL',
+			'name'          => 'varchar(100) NOT NULL',
+			'code'          => 'varchar(50) NOT NULL',
+			'description'   => 'varchar(1000)',
+		));
+
+		$this->createTable('users', array(
+			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+			'owner_id'      => 'int(11) UNSIGNED NOT NULL',
+			'name'          => 'varchar(100) NOT NULL',
+			'email'         => 'varchar(100) NOT NULL',
+			'password'      => 'varchar(50) NOT NULL',
+			'status'        => 'varchar(50) NOT NULL',
+			'last_activity' => 'int(11) UNSIGNED',
+		));
+
+		$this->createTable('user_has_project', array(
+			'user_id'       => 'int(11) UNSIGNED NOT NULL',
+			'project_id'    => 'int(11) UNSIGNED NOT NULL',
+		));
+
 		$this->createTable('steps', array(
 			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
 			'name'          => 'varchar(100) NOT NULL',
@@ -13,6 +59,11 @@ class m131112_043856_index extends CDbMigration
 			'order_by'      => 'int(5) UNSIGNED NOT NULL COMMENT \'Sort order\'',
 		));
 
+		$this->createTable('step_has_step', array(
+			'step_from'     => 'int(11) UNSIGNED NOT NULL',
+			'step_to'       => 'int(11) UNSIGNED NOT NULL',
+		));
+
 		$this->createTable('types', array(
 			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
 			'name'          => 'varchar(100) NOT NULL',
@@ -20,6 +71,15 @@ class m131112_043856_index extends CDbMigration
 			'code'          => 'varchar(20) NOT NULL',
 			'description'   => 'varchar(1000)',
 			'order_by'      => 'int(5) UNSIGNED NOT NULL COMMENT \'Sort order\'',
+		));
+
+		$this->createTable('comments', array(
+			'id'            => 'int(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+			'user_id'       => 'int(11) UNSIGNED NOT NULL',
+			'issue_id'      => 'int(11) UNSIGNED NOT NULL',
+			'time'          => 'int(11) UNSIGNED NOT NULL',
+			'type'          => 'varchar(20) NOT NULL',
+			'description'   => 'varchar(1000)',
 		));
 
 
@@ -124,12 +184,152 @@ class m131112_043856_index extends CDbMigration
 		));
 
 
+		$this->insert('projects', array(
+			'owner_id'      => 1,
+			'name'          => 'Test project',
+			'code'          => 'test',
+			'description'   => 'Тестовый проект',
+		));
+
+
+		$this->insert('users', array(
+			'owner_id'      => 0,
+			'name'          => 'User 1',
+			'email'         => 'demo@demo.demo',
+			'password'      => 'fe01ce2a7fbac8fafaed7c982a04e229',
+			'status'        => 'worked',
+			'last_activity' => 0,
+		));
+
+		$this->insert('users', array(
+			'owner_id'      => 0,
+			'name'          => 'User 2',
+			'email'         => 'user@demo.demo',
+			'password'      => 'fe01ce2a7fbac8fafaed7c982a04e229',
+			'status'        => 'worked',
+			'last_activity' => 0,
+		));
+
+
+		$this->insert('user_has_project', array(
+			'user_id'       => 1,
+			'project_id'    => 1,
+		));
+
+		$this->insert('user_has_project', array(
+			'user_id'       => 2,
+			'project_id'    => 1,
+		));
+
+
+		$this->insert('issues', array(
+			'step_id'       => 1,
+			'project_id'    => 1,
+			'assigned_to'   => 2,
+			'owner_id'      => 1,
+			'parent_id'     => 0,
+			'name'          => 'Test bug issue',
+			'type'          => 'bug',
+			'description'   => 'Its demo issue',
+			'rep_steps'     => 'No rep steps',
+			'number'        => 1,
+			'added_date'    => time() - (3600 * 24 * 2),
+			'dedline_date'  => time() + (3600 * 24 * 5),
+			'last_activity' => time() - 3600,
+		));
+
+		$this->insert('issues', array(
+			'step_id'       => 1,
+			'project_id'    => 1,
+			'assigned_to'   => 2,
+			'owner_id'      => 1,
+			'parent_id'     => 1,
+			'name'          => 'Feature request issue',
+			'type'          => 'featurerequest',
+			'description'   => 'Its demo fr',
+			'rep_steps'     => '',
+			'number'        => 2,
+			'added_date'    => time() - (3600 * 24 * 1),
+			'dedline_date'  => time() + (3600 * 24 * 10),
+			'last_activity' => time() - 1800,
+		));
+
+		$this->insert('issues', array(
+			'step_id'       => 1,
+			'project_id'    => 1,
+			'assigned_to'   => 2,
+			'owner_id'      => 1,
+			'parent_id'     => 1,
+			'name'          => 'Enhancement item',
+			'type'          => 'enhancement',
+			'description'   => 'enhancement',
+			'rep_steps'     => '',
+			'number'        => 3,
+			'added_date'    => time(),
+			'dedline_date'  => 0,
+			'last_activity' => time() - 600,
+		));
+
+		$this->insert('issues', array(
+			'step_id'       => 1,
+			'project_id'    => 1,
+			'assigned_to'   => 2,
+			'owner_id'      => 1,
+			'parent_id'     => 1,
+			'name'          => 'Test Task issue',
+			'type'          => 'task',
+			'description'   => 'Its demo issue',
+			'rep_steps'     => '',
+			'number'        => 4,
+			'added_date'    => time() - (3600 * 24 * 1),
+			'dedline_date'  => 0,
+			'last_activity' => time() - 600,
+		));
+
+		$this->insert('issues', array(
+			'step_id'       => 1,
+			'project_id'    => 1,
+			'assigned_to'   => 2,
+			'owner_id'      => 1,
+			'parent_id'     => 2,
+			'name'          => 'Test other issue',
+			'type'          => 'other',
+			'description'   => 'Its demo issue',
+			'rep_steps'     => '',
+			'number'        => 5,
+			'added_date'    => time() - (3600 * 24 * 1),
+			'dedline_date'  => 0,
+			'last_activity' => time() - 600,
+		));
+
+		$this->insert('issues', array(
+			'step_id'       => 1,
+			'project_id'    => 1,
+			'assigned_to'   => 2,
+			'owner_id'      => 1,
+			'parent_id'     => 2,
+			'name'          => 'Test idea issue',
+			'type'          => 'idea',
+			'description'   => 'Its idea issue',
+			'rep_steps'     => '',
+			'number'        => 6,
+			'added_date'    => time() - (3600 * 24 * 1),
+			'dedline_date'  => 0,
+			'last_activity' => time() - 600,
+		));
 	}
 
 	public function down()
 	{
+		$this->dropTable('issues');
+		$this->dropTable('issue_has_issue');
+		$this->dropTable('projects');
+		$this->dropTable('users');
+		$this->dropTable('user_has_project');
 		$this->dropTable('steps');
+		$this->dropTable('step_has_step');
 		$this->dropTable('types');
+		$this->dropTable('comments');
 	}
 
 	// Use safeUp/safeDown to do migration with transaction
