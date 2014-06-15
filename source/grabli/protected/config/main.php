61fa70f4b9671da860fb471b956dae8545e423b4
Ilja <the.ilja@gmail.com>
2014-06-15 10:10:15 +0400

    some config

--- source/grabli/protected/config/main.php
+++ source/grabli/protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'GRABL! the open bug tracker',
+    'timeZone' => 'Europe/Moscow',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
